6b50c8fb510ad0cd19290a076b4b735bfab40a2d
Helen.Ezike <cheirihelen@gmail.com>
2024-07-20 06:27:36 -0700

    initial commit

--- quiz.js
+++ quiz.js
@@ -4,15 +4,15 @@ function checkAnswer() {
     const correctAnswer = "4";
 
     // Retrieve the user's selected answer
-    const userAnswer = document.querySelector('input[name="quiz"]:checked');
+    const userAnswerElement = document.querySelector('input[name="quiz"]:checked');
 
     // Check if the user has selected an answer
-    if (userAnswer) {
+    if (userAnswerElement) {
         // Get the value of the selected answer
-        const selectedAnswer = userAnswer.value;
+        const userAnswer = userAnswerElement.value;
 
         // Compare the user's answer with the correct answer
-        if (selectedAnswer === correctAnswer) {
+        if (userAnswer === correctAnswer) {
             document.getElementById('feedback').textContent = "Correct! Well done.";
         } else {
             document.getElementById('feedback').textContent = "That's incorrect. Try again!";
@@ -25,4 +25,3 @@ function checkAnswer() {
 
 // Add event listener to the "Submit Answer" button
 document.getElementById('submit-answer').addEventListener('click', checkAnswer);
-
